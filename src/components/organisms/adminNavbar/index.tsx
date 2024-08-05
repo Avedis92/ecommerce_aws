231c44ad3718fc7e497e32bac9ca41f34558d418
Avedis Demirdjian <avodem73@gmail.com>
2024-08-05 20:55:09 +0400

    Update styles

--- src/components/organisms/adminNavbar/index.tsx
+++ src/components/organisms/adminNavbar/index.tsx
@@ -1,16 +1,14 @@
 import NavLeftContent from "../../molecules/navLeftContent";
-import styles from "./style.module.css";
 import useAuth from "../../../hooks/useAuth";
 import UserSettings from "../../molecules/userSettings";
 import useNavbar from "../../../hooks/useNavbar";
 
 const AdminNavbar = () => {
-  const { container } = styles;
   const { authUser, signOut } = useAuth();
   const { pathname } = useNavbar();
 
   return (
-    <nav className={container}>
+    <nav className="flex justify-between items-center py-0 px-8 bg-white">
       <NavLeftContent title="Men's Wear" subtitle="Admin panel" />
       {authUser && (
         <UserSettings
