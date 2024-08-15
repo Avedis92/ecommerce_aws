2d18a8985f81c9e7717f5eddc01fbb0cc2722993
Avedis Demirdjian <avodem73@gmail.com>
2024-08-15 14:57:23 +0400

    update config file

--- vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_AWS_API_GATEWAY_BASE_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
 // http://localhost:3000
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: {},
-  },
-  server: {
-    proxy: {
-      "/api/v1": {
-        target: import.meta.env["VITE_AWS_API_GATEWAY_BASE_URL"],
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    define: {
+      global: {},
+    },
+    server: {
+      proxy: {
+        "/api/v1": {
+          target: env["VITE_AWS_API_GATEWAY_BASE_URL"],
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
