5e4a875f70ef73aff196871bbf775e35587fa00e
Avedis Demirdjian <avodem73@gmail.com>
2024-08-15 14:42:17 +0400

    fix ts config file issue

---
+++ vite-env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly VITE_AWS_API_GATEWAY_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 // http://localhost:3000
@@ -14,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/v1": {
-        target: process.env["VITE_AWS_API_GATEWAY_BASE_URL"],
+        target: import.meta.env["VITE_AWS_API_GATEWAY_BASE_URL"],
         changeOrigin: true,
       },
     },
