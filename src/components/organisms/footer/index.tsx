e57d0517c95ba9fdf10ca0508719c7a226c306fa
Avedis Demirdjian <avodem73@gmail.com>
2024-08-18 00:05:37 +0400

    Add footer

---
+++ src/components/organisms/footer/index.tsx
@@ -0,0 +1,28 @@
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { FiTwitter } from "react-icons/fi";
+import { FiYoutube } from "react-icons/fi";
+import NavLeftContent from "../../molecules/navLeftContent";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white flex flex-col items-center justify-between py-8 px-0">
+      <NavLeftContent title="Men's Wear" />
+      <ul className="list-none flex items-center gap-y-8 p-0">
+        <li className="cursor-pointer">Company</li>
+        <li className="cursor-pointer">Products</li>
+        <li className="cursor-pointer">Offices</li>
+        <li className="cursor-pointer">About</li>
+        <li className="cursor-pointer">Contact</li>
+      </ul>
+      <div className="flex items-center gap-y-8 p-0">
+        <FaInstagram size="2rem" className="cursor-pointer" />
+        <CiFacebook size="2rem" className="cursor-pointer" />
+        <FiTwitter size="2rem" className="cursor-pointer" />
+        <FiYoutube size="2rem" className="cursor-pointer" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
